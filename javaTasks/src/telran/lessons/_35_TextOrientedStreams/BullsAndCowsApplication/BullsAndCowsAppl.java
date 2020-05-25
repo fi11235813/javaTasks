9edb75b0195008576889fcd33ee219cbce938e4b
fi11235813 <fi11235813@gmail.com>
2020-05-25 22:17:49 +0300

    HomeWork: BullsAndCowsApplication

---
+++ javaTasks/src/telran/lessons/_35_TextOrientedStreams/BullsAndCowsApplication/BullsAndCowsAppl.java
@@ -0,0 +1,11 @@
+package telran.lessons._35_TextOrientedStreams.BullsAndCowsApplication;
+
+public class BullsAndCowsAppl {
+
+	public static void main(String[] args) {
+		BullsAndCows BC = new BullsAndCows();
+		BC.play();
+
+	}
+
+}
---
+++ javaTasks/src/telran/lessons/_35_TextOrientedStreams/BullsAndCowsApplication/BullsCowsEnum.java
@@ -0,0 +1,15 @@
+package telran.lessons._35_TextOrientedStreams.BullsAndCowsApplication;
+
+public enum BullsCowsEnum {
+	EXIT("exit"), TEST_MODE("test"), COWS("cows"), BULLS("bulls");
+	
+	String value;
+	
+	BullsCowsEnum(String value) {
+		this.value = value;
+	}
+	
+	public String getValue() {
+		return this.value;
+	}
+}
