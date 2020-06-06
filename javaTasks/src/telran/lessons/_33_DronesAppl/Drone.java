a56feb4f41a6e28a01c96b125109102e32c349b2
fi11235813 <fi11235813@gmail.com>
2020-06-06 15:52:03 +0300

    HomeWork: DronesApplication

--- javaTasks/src/telran/lessons/_33_DronesAppl/Drone.java
+++ javaTasks/src/telran/lessons/_33_DronesAppl/Drone.java
@@ -1,9 +1,8 @@
 package telran.lessons._33_DronesAppl;
 
 public class Drone {
-	
+
 	public static int nIteration = 0;
-	
 	private final int seqNumber;
 	private Height height;
 	private int nPassengers;
@@ -12,69 +11,70 @@ public class Drone {
 	private int startQueueIteration;
 	private int totalAirIterations;
 	private int totalQueueIterations;
+
 	public Drone(int seqNumber) {
 		this.seqNumber = seqNumber;
 	}
-	
+
 	public void setHeight(Height height) {
 		height.addVisiter();
 		this.height = height;
 	}
-	
+
 	public boolean isFinishIteration() {
 		return finishIteration == nIteration;
 	}
-	
+
 	public int getnPassengers() {
 		return nPassengers;
 	}
-	
+
 	public void takeOff() {
 		off();
 		startIteration = nIteration;
 		nPassengers++;
-		
+
 	}
-	
+
 	public void off() {
-		totalQueueIterations += (nIteration - startQueueIteration); 
+		totalQueueIterations += (nIteration - startQueueIteration);
 	}
-	
+
 	public void landingOn() {
-		totalAirIterations += (nIteration -  startIteration);
+		totalAirIterations += (nIteration - startIteration);
 		startQueueIteration = nIteration;
 	}
 
 	public void setFinishIteration(int finishIteration) {
 		this.finishIteration = nIteration + finishIteration;
 	}
-	
+
 	public int getTotalAirIterations() {
 		return totalAirIterations;
 	}
-	
+
 	public int getTotalQueueIterations() {
 		return totalQueueIterations;
 	}
-	
+
 	public int getSeqNumber() {
 		return seqNumber;
-	
+
 	}
-	
+
 	public Height clearHeight() {
 		height.removeObjectsOnHeight();
 		return height;
 	}
-	
+
 	public int getStartQueueIteration() {
 		return startQueueIteration;
 	}
-	
+
 	@Override
 	public String toString() {
-		return String.format("Drone Id: %d in air: %d min. transferred: %d pas. in waiting queue: %d min",
-				seqNumber, totalAirIterations, nPassengers, totalQueueIterations);
+		return String.format("Drone Id: %d in air: %d min. transferred: %d pas. in waiting queue: %d min", seqNumber,
+				totalAirIterations, nPassengers, totalQueueIterations);
 	}
-	
+
 }
--- javaTasks/src/telran/lessons/_33_DronesAppl/DronesAppl.java
+++ javaTasks/src/telran/lessons/_33_DronesAppl/DronesAppl.java
@@ -1,9 +1,8 @@
 package telran.lessons._33_DronesAppl;
+
 import java.util.*;
 import java.util.stream.Collectors;
 import java.util.stream.IntStream;
-
-
 
 public class DronesAppl {
 	private static final int N_DRONES = 20;
@@ -14,159 +13,154 @@ public class DronesAppl {
 	static Drone[] drones;
 	static List<Drone> dronesInAir;
 	static Queue<Drone> dronesInQueue;
-	static final Height[] heights = new Height[N_HEIGHTS];	
+	static final Height[] heights = new Height[N_HEIGHTS];
 
-		public static void main(String[] args) {
-			preProcessing();
-			play();
-			postProcessing();
-			displayResults();
+	public static void main(String[] args) {
+		preProcessing();
+		play();
+		postProcessing();
+		displayResults();
+
+	}
+
+	private static void displayResults() {
+		displayDronesInfo();
+		displayHeightsCountsInfo();
+		displayMostPopularHeights();
+	}
+
+	private static void displayMostPopularHeights() {
+		List<Height> list = Arrays.stream(heights).collect(Collectors.groupingBy(i -> i.getVisitCounts())).entrySet()
+				.stream().max((i, j) -> Integer.compare(i.getKey(), j.getKey())).stream()
+				.flatMap(h -> h.getValue().stream()).collect(Collectors.toList());
+
+		System.out.println(list);
+	}
+
+	private static void displayHeightsCountsInfo() {
+		for (Height height : heights) {
+			System.out.println(String.format("%s: %d flights", height, height.getVisitCounts()));
+		}
+	}
+
+	private static void displayDronesInfo() {
+		for (Drone drone : drones) {
+			System.out.println(drone.toString());
+		}
+	}
+
+	private static void postProcessing() {
+		landingAllDrones();
+		closingShop();
+	}
+
+	private static void closingShop() {
+		Iterator<Drone> it = dronesInQueue.iterator();
+		while (it.hasNext()) {
+			Drone drone = it.next();
+			drone.off();
+			it.remove();
+		}
+	}
+
+	private static void landingAllDrones() {
+		Iterator<Drone> it = dronesInAir.iterator();
+		while (it.hasNext()) {
+			Drone drone = it.next();
+			drone.landingOn();
+			it.remove();
+		}
+	}
+
+	private static void play() {
+		for (int i = 1; i <= MODEL_TIME; i++) {
+			Drone.nIteration++;
+			List<Height> freedHeights = landingOnIteration();
+			takingOffOnIteration(freedHeights);
 
 		}
 
-		private static void displayResults() {
-			displayDronesInfo();
-			displayHeightsCountsInfo();
-			displayMostPopularHeights();
+	}
+
+	private static void takingOffOnIteration(List<Height> freedHeights) {
+		if (freedHeights.isEmpty()) {
+			return;
 		}
 
-		private static void displayMostPopularHeights() {
-			List<Height> list = Arrays.stream(heights).collect(Collectors.groupingBy(i -> i.getVisitCounts())).entrySet()
-			.stream().max((i, j) -> Integer.compare(i.getKey(), j.getKey())).stream()
-			.flatMap(h -> h.getValue().stream()).collect(Collectors.toList());
-			
-			System.out.println(list);
-}
-
-		private static void displayHeightsCountsInfo() {
-			for(Height height : heights) {
-				System.out.println(String.format("%s: %d flights", height, height.getVisitCounts()));
-			}
+		for (Height height : freedHeights) {
+			takeOff(dronesInQueue.remove(), height);
 		}
+	}
 
-		private static void displayDronesInfo() {
-			for(Drone drone : drones) {
-				System.out.println(drone.toString());
-			}
-		}
-
-		private static void postProcessing() {
-			landingAllDrones();
-			closingShop();
-		}
-
-		private static void closingShop() {
-			Iterator<Drone> it = dronesInQueue.iterator(); 
-			while(it.hasNext()) {
-				Drone drone = it.next();
-				drone.off();
+	private static List<Height> landingOnIteration() {
+		List<Height> res = new ArrayList<>();
+		Iterator<Drone> it = dronesInAir.iterator();
+		while (it.hasNext()) {
+			Drone drone = it.next();
+			if (drone.isFinishIteration()) {
 				it.remove();
-			}
-		}
-
-		private static void landingAllDrones() {
-			Iterator<Drone> it = dronesInAir.iterator(); 
-			while(it.hasNext()) {
-				Drone drone = it.next();
 				drone.landingOn();
-				it.remove();
+				putInQueue(drone);
+				res.add(drone.clearHeight());
 			}
 		}
+		return res;
+	}
 
-		private static void play() {
-			for (int i = 1; i <= MODEL_TIME; i++) {
-				Drone.nIteration++;
-				List<Height> freedHeights = landingOnIteration();
-				takingOffOnIteration(freedHeights);
-				
-			}
-			
-		}
+	private static void preProcessing() {
+		createDrones();
+		createHeights();
+		initializeInAir();
+		startDronesInAir();
+		startDronesInQueue();
 
-		private static void takingOffOnIteration(List<Height> freedHeights) {
-			if (freedHeights.isEmpty()) {
-				return;
-			}
-			
-			for (Height height : freedHeights) {
-					takeOff(dronesInQueue.remove(), height);
-			}
+	}
+
+	private static void startDronesInQueue() {
+		for (int i = N_HEIGHTS; i < N_DRONES; i++) {
+			putInQueue(drones[i]);
 		}
 
-		private static List<Height> landingOnIteration() {
-			List<Height> res = new ArrayList<>();
-			Iterator<Drone> it = dronesInAir.iterator();
-			while(it.hasNext()) {
-				Drone drone = it.next();
-				if (drone.isFinishIteration()) {
-					it.remove();
-					drone.landingOn();
-					putInQueue(drone);
-					res.add(drone.clearHeight());
-				}
-			}
-			return res;
+	}
+
+	private static void putInQueue(Drone drone) {
+		dronesInQueue.add(drone);
+	}
+
+	private static void startDronesInAir() {
+		for (int i = 0; i < N_HEIGHTS; i++) {
+			takeOff(drones[i], heights[i]);
 		}
 
-		private static void preProcessing() {
-			createDrones();
-			createHeights();
-			initializeInAir();
-			startDronesInAir();
-			startDronesInQueue();
-			
-		}
+	}
 
-		private static void startDronesInQueue() {
-			for(int i = N_HEIGHTS; i < N_DRONES; i++) {
-				putInQueue(drones[i]);
-			}
-			
-		}
+	private static void takeOff(Drone drone, Height height) {
+		dronesInAir.add(drone);
+		int iterationsInAir = getIterationsInAir();
+		drone.setFinishIteration(iterationsInAir);
+		drone.setHeight(height);
+		drone.takeOff();
+	}
 
-		private static void putInQueue(Drone drone) {
-			dronesInQueue.add(drone);
-		}
+	private static int getIterationsInAir() {
 
-		private static void startDronesInAir() {
-			for (int i = 0; i < N_HEIGHTS; i++) {
-				takeOff(drones[i], heights[i]);
-			}
-			
-		}
+		return (int) (MIN_PASSENGER_TIME + Math.random() * (MAX_PASSENGER_TIME - MIN_PASSENGER_TIME + 1));
+	}
 
-		private static void takeOff(Drone drone, Height height) {
-			dronesInAir.add(drone);
-			int iterationsInAir = getIterationsInAir();
-			drone.setFinishIteration(iterationsInAir);
-			drone.setHeight(height);
-			drone.takeOff();
-		}
+	private static void initializeInAir() {
+		dronesInAir = new LinkedList<Drone>();
+		dronesInQueue = new PriorityQueue<>(
+				(e1, e2) -> Integer.compare(e1.getStartQueueIteration(), e2.getStartQueueIteration()));
+	}
 
-		private static int getIterationsInAir() {
-			
-			return (int) (MIN_PASSENGER_TIME +
-					Math.random() * (MAX_PASSENGER_TIME -
-							MIN_PASSENGER_TIME + 1));
-		}
+	private static void createDrones() {
+		drones = IntStream.range(1, N_DRONES + 1).mapToObj(i -> new Drone(i)).toArray(Drone[]::new);
+	}
 
-		
-		
-		private static void initializeInAir() {
-			dronesInAir = new LinkedList<Drone>();
-			dronesInQueue = new PriorityQueue<>((e1, e2) -> Integer.compare(e1.getStartQueueIteration(), e2.getStartQueueIteration()));
+	private static void createHeights() {
+		for (int i = 0; i < N_HEIGHTS; i++) {
+			heights[i] = new Height("Height", i + 1);
 		}
-		
-		private static void createDrones() {
-			drones = IntStream.range(1, N_DRONES + 1).mapToObj(i -> new Drone(i)).toArray(Drone[]::new);
-		}
-		
-		private static void createHeights() {
-			for(int i = 0; i < N_HEIGHTS; i++) {
-				heights[i] = new Height("Height", i + 1);
-			}
-		}
-		
-
+	}
 
 }
